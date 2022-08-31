a6a46c3244afaa1c7f596e4f7535e4771d56197c
Maxim1M <maksim_mikhaylov1810@mail.ru>
2022-08-31 21:30:43 +0300

    add

--- CodeWars.js
+++ CodeWars.js
@@ -1,7 +1,20 @@
+/*
+
+ //
 function even_or_odd(n) {
+
   if(n % 2 === 0) {
     return 'Even';
   } else {
     return 'Odd';
   }
 }
+
+function
+
+//
+
+function reverseWords(str){
+  return str.split(" ").reverse().join(" ") // reverse those words
+}
+ */
