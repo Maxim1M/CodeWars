90d497fbe9735ff6924361fccf4b90eaedc92dc6
Maxim1M <maksim_mikhaylov1810@mail.ru>
2022-09-05 12:31:25 +0300

    Given a non-empty array of integers, return the result of multiplying the values together in order. Example:

--- CodeWars.js
+++ CodeWars.js
@@ -30,5 +30,16 @@ var summation = function (num) {
 
 //
 
+function grow(x){
+  let result = x[0];
+  for (let i = 1; i < x.length; i++) {
+    result *= x[i];
+  }
+  return result;
+}
+
+//
+
+
 
  */
