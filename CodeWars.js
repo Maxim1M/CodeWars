e173d9917f1a788e0c6a62444f1c0a7a98d8092d
Maxim <maksim_mikhaylov1810@mail.ru>
2022-09-14 14:21:28 +0300

    Sum of positive

--- CodeWars.js
+++ CodeWars.js
@@ -151,11 +151,14 @@ function rentalCarCost(d) {
 
 // 16
 
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] > 0) sum += arr[i]
+  }
+  return sum
+}
 
-
-
-
-
-//
+// 17
 
  */
