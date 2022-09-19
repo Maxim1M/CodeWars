b6daa29233f722a9c636cea7d14300ca5858048e
Maxim <maksim_mikhaylov1810@mail.ru>
2022-09-19 19:43:55 +0300

    Square Every Digit

--- CodeWars.js
+++ CodeWars.js
@@ -337,5 +337,16 @@ function disemvowel(str) {
   return a;
 }
 
+// 39
+
+function squareDigits(num){
+  let string = num.toString();
+  let results = [];
+  for (let i = 0; i < string.length; i++){
+      results[i] = string[i] * string[i];
+  }
+  return Number(results.join(''));
+}
+
 //
  */
